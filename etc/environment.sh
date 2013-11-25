3eb2b34edec7b5070408051d737523b7c8bcd9a5
Kamalesh Kalarickal <kamalesh.kalarickal@wisertogether.com>
2013-11-25 16:05:21 -0500

    Fix reference to ETC_DIR with PROJECT_HOME_DIR/etc

--- etc/environment.sh
+++ etc/environment.sh
@@ -11,11 +11,11 @@
 
 # bring in per-project configurations
 if [ -f "$ETC_DIR/project-environment.sh" ]; then
-    source $ETC_DIR/project-environment.sh
+    source $PROJECT_HOME_DIR/etc/project-environment.sh
 fi
 
 # bring in local configurations
 if [ -f "$ETC_DIR/local-environment.sh" ]; then
-    source $ETC_DIR/local-environment.sh
+    source $PROJECT_HOME_DIR/etc/local-environment.sh
 fi
 
